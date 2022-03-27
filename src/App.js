ec9bc31e7965f2466841a87f0b9d72cc6b39ef64
vishal singh <vishurajputmgr@gmail.com>
2022-03-27 09:13:44 +0530

    card, footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Section from './component/body/section';
+import Footer from './component/footer/footer';
 import NavBar from './component/navbar/navBar';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       {/* hello dc divakar */}
       <NavBar />
       <Section />
+      <Footer />
     </div>
   );
 }
---
+++ src/component/footer/footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './footer.css';
+
+function Footer() {
+  return (
+    <>
+    <div className="footer">
+        <div className="container">
+            <button className="btn">HOME</button>
+            <button className="btn">SEARCH</button>
+            <button className="btn">PROFILE</button>
+            <button className="btn">ADD</button>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Footer
