05e815c038bb0b5da3c28c7b9c62c94a49fa5cc2
vishal singh <vishurajputmgr@gmail.com>
2022-03-27 11:00:47 +0530

    added footer

--- src/component/card/card.jsx
+++ src/component/card/card.jsx
@@ -7,19 +7,38 @@ function Card() {
     <div className="card">
         <div className="card-content">
         <div className="card-header">
-        <div className="cardName">
-            <h2>Name kumar</h2>
-            <h5>userName</h5>
-        </div>
-            <div className="topRight">
-            <p>logout</p>
-            <img src="logo.png" alt="dp" />
+            <div className="cardName">
+                <h2>Name kumar</h2>
+                <h5>userName</h5>
             </div>
-            <div className="block">
+            <div className="topRight">
+                <p>profile</p>
+                <img src="logo.png" alt="dp" />
+            </div>
+        </div><hr className="hr" />
+        <div className="block">
                 <h2>twitter</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis quia quae, eaque excepturi vitae dolorum magnam, laboriosam id nulla harum alias, praesentium obcaecati voluptate soluta nemo iste tempora esse vero!</p>
             </div>
+            <div className="block1">
+                <h2>github</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis quia quae, eaque excepturi vitae dolorum magnam, laboriosam id nulla harum alias, praesentium obcaecati voluptate soluta nemo iste tempora esse vero!</p>
+            </div>
+            <div className="block2">
+                <h2>insta</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis quia quae, eaque excepturi vitae dolorum magnam, laboriosam id nulla harum alias, praesentium obcaecati voluptate soluta nemo iste tempora esse vero!</p>
+            </div><hr />
         </div>
+        <div className="social">
+            <div>
+                <h5>like</h5>
+            </div>
+            <div>
+                <h5>comment</h5>
+            </div>
+            <div>
+                <h5>share  </h5>
+            </div>
         </div>
     </div>
     </>
